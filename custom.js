7590b35a8157dfd9b07846df0fb3ed8dd5d87d44
Jose Diaz-Gonzalez <osedread@gmail.com>
2017-05-15 18:28:24 +0100

    Redesign of the landing and navbar of the page

--- custom.js
+++ custom.js
@@ -1,14 +1,9 @@
 $(document).ready( function() {
 
-	// Quick fix for collapsing the navbar after pressing one of the sections
-	$('nav a').click(function(){
-   		$('nav button').click();
-	});
+	//
+	$(".nav-btn").on("click", function(){
 
-	// When the user scrolls down, the navbar should fade out and the responsive button should fade in
-	/* $(window).scroll( function() {
-		if ($(window).scrollTop() > 0) {
-		});
-	*/
+		$(".nav-content").slideToggle("slow");
+	});
 
 });
