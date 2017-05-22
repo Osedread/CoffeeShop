e712969f95de2d474af30a6e90677d91de11b4ac
Jose Diaz-Gonzalez <osedread@gmail.com>
2017-05-22 21:33:25 +0100

    Implemented scss in the project, minified css and js with Grunt, changed landing, navbar and color palette

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+var $section = $("section");
+var $h3 = $("h3");
+
+$h3.hide();
+
+$(document).ready( function() {
+
+	//
+	$(".nav-btn").on("click", function(){
+		$(".nav-content").slideToggle("slow");
+	});
+
+	$section.on("inview", function(event, isInView) {
+		if (isInView) {
+			$section.find("h3").fadeIn();
+		}
+	});
+
+});
